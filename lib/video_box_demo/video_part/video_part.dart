c102b9b72f27dbb6b6a9098d4f213c7c870fdca2
caoduczpsm <nguyencaoduc0922@gmail.com>
2023-07-13 09:27:20 +0700

    delete comment in video_part

--- lib/video_box_demo/video_part/video_part.dart
+++ lib/video_box_demo/video_part/video_part.dart
@@ -5,37 +5,20 @@ import 'package:chewie/chewie.dart';
 import 'package:get/get.dart';
 import 'package:video_box_demo/video_box_demo/video_part/video_part_controller.dart';
 import '../../models/video_model.dart';
-import '../../ults/constants.dart';
 
 class VideoPart extends StatelessWidget {
   const VideoPart({Key? key, required this.videos}) : super(key: key);
 
   final List<Video> videos;
 
-  final textTitleStyle = const TextStyle(
-      fontSize: Constants.textTitleSize,
-      fontWeight: FontWeight.bold,
-      fontFamily: Constants.fontName);
-  final textDescriptionStyle = const TextStyle(
-      fontSize: Constants.textBodySize, fontFamily: Constants.fontName);
-  final currentPageTextStyle = const TextStyle(
-      fontSize: Constants.textBodySize,
-      color: Colors.blueAccent,
-      fontWeight: FontWeight.bold,
-      fontFamily: Constants.fontName);
-
   @override
   Widget build(BuildContext context) {
-    // Use Get.put() to create and get the instance of VideoController
-    final VideoPartController videoController = Get.put(VideoPartController());
 
-    // Call initState() to initialize the controllers
+    final VideoPartController videoController = Get.put(VideoPartController());
     videoController.initState(videos);
 
     return GetBuilder<VideoPartController>(
-      // Pass the controller to the init parameter
       init: videoController,
-      // Return a widget in the builder parameter
       builder: (controller) {
         return Center(
           child: AnimatedSwitcher(
