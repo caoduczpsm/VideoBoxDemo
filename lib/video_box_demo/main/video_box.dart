70d6838b36eb28ff3afaf58e817a5b9e924d6d90
caoduczpsm <nguyencaoduc0922@gmail.com>
2023-07-14 16:49:51 +0700

    division of display areas on the screen

--- lib/video_box_demo/main/video_box.dart
+++ lib/video_box_demo/main/video_box.dart
@@ -4,10 +4,16 @@ import 'package:video_box_demo/video_box_demo/main/video_box_controller.dart';
 import 'package:video_box_demo/video_box_demo/sos_news/sos_news.dart';
 // ignore: depend_on_referenced_packages
 import 'package:get/get.dart';
+import 'package:video_box_demo/video_box_demo/video_part/video_part.dart';
 import '../news_feed/news_feed_part.dart';
 
 class VideoBox extends StatelessWidget {
   const VideoBox({Key? key}) : super(key: key);
+
+  static const int fullScreenMode = 0;
+  static const int notifyMode = 1;
+  static const int leftMode = 2;
+  static const int rightMode = 3;
 
   @override
   Widget build(BuildContext context) {
@@ -25,46 +31,70 @@ class VideoBox extends StatelessWidget {
               child: CircularProgressIndicator(),
             );
           }
-          return Row(
-            children: [
-              Expanded(
-                flex: 2,
-                child: Column(
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: Obx(() {
-                        if (controller.isShowingNewsFeed.value) {
-                          return NewsFeedPart(news: controller.news);
-                        } else {
-                          return SosNews(message: controller.message!);
-                        }
-                      }),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(top: 20, bottom: 70),
-                child: const VerticalDivider(
-                  thickness: 1.0,
-                  color: Colors.grey,
-                ),
-              ),
-              Expanded(
-                flex: 3,
-                child: Container(
-                  margin: const EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    color: Colors.lightBlue[100],
-                    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+          return Obx(() {
+
+            Widget leftPart, rightPart, notifyPart, fullScreen;
+
+            if (!controller.isShowingNewsFeed.value) {
+              int vungPhat = int.parse(controller.message!.payload!.VungPhat!);
+
+              if (vungPhat == fullScreenMode) {
+                fullScreen = SosNews(message: controller.message!);
+                leftPart = Container();
+                rightPart = Container();
+              } else if (vungPhat == leftMode) {
+                leftPart =  SosNews(message: controller.message!);
+                rightPart = VideoPart(videos: controller.videos);
+                //Tạm
+                rightPart = Container();
+              } else { // rightMode
+                rightPart = SosNews(message: controller.message!);
+                leftPart = NewsFeedPart(news: controller.news);
+              }
+            } else {
+              leftPart = NewsFeedPart(news: controller.news);
+              rightPart = VideoPart(videos: controller.videos);
+              //Tạm
+              rightPart = Container();
+            }
+
+            fullScreen = Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: leftPart,
+                      ),
+                    ],
                   ),
-                  //child: VideoPart(videos: controller.videos,),
-                  child: Container(),
                 ),
-              ),
-            ],
-          );
+                Container(
+                  padding: const EdgeInsets.only(top: 20, bottom: 70),
+                  child: const VerticalDivider(
+                    thickness: 1.0,
+                    color: Colors.grey,
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlue[100],
+                      borderRadius:
+                      const BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                    //child: VideoPart(videos: controller.videos,),
+                    child: rightPart,
+                  ),
+                ),
+              ],
+            );
+            return fullScreen;
+          });
         },
       ),
     );
